67da6eeeecd48ba788d60352f45054dfe6f9e715
Bryan P. Arant <brynarnt@protonmail.com>
2019-10-19 16:15:54 -0700

    added clean/run; buggy implementations but this is an experiment

--- build.py
+++ build.py
@@ -3,13 +3,30 @@ import glob
 import subprocess;
 
 EXE_NAME = "useless-log.exe"
-# working directory
-print("Current Working Directory = " + os.getcwd())
 
-cpp_files = glob.glob(os.getcwd() + "/*.cpp")
-subprocess.run(["clang", "-c"] + cpp_files)
+def build():
+    # working directory
+    print("Current Working Directory = " + os.getcwd())
 
-o_files = glob.glob(os.getcwd() + "/*.o")
-subprocess.run(["clang"] + o_files + ["-o", EXE_NAME])
+    # Note, this will only perform build/linking on the files in the current directory.
+    cpp_files = glob.glob(os.getcwd() + "/*.cpp")
+    subprocess.run(["clang", "-c"] + cpp_files)
+    print("Building o files from " + str(cpp_files))
 
-print("Built " + EXE_NAME)
+    o_files = glob.glob(os.getcwd() + "/*.o")
+    subprocess.run(["clang"] + o_files + ["-o", EXE_NAME])
+    print("Building executable from " + str(o_files))
+
+    print("Built " + EXE_NAME)
+
+def clean():
+    cleanup_files = glob.glob(os.getcwd() + "/*.txt") + glob.glob(os.getcwd() + "/*.o") + glob.glob(os.getcwd() + "/*.exe")
+    for file in cleanup_files:
+        print("removing " + file + "...")
+        os.remove(file)
+
+    print("Done with cleanup.");
+
+def run():
+    print("Current working directory = " + os.getcwd())
+    subprocess.run([EXE_NAME])
--- log.cpp
+++ log.cpp
@@ -19,6 +19,9 @@ namespace Lab
         using namespace std;
         fstream file;
 
+        cout << "Just making a change to test workflow...here." << endl;
+        cout << "Could theoretically do continuous building with this by monitoring .o files for changes..." << endl;
+        cout << "It's kind of fun. Then python becomes my command line, of sorts." << endl;
         cout << "Writing to " << _filename.c_str() << endl;
         
         file.open(_filename.c_str(), ios_base::out);
